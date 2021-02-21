223e22f6376a2145d614239bda55ef88d143a4e4
Jonathan Perry <jpper@umich.edu>
2021-02-21 11:00:28 +0900

    Added ios platform and finished styling the dummy audio player UI

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-
+  @ViewChild(IonTabs) tabs;
+  selected = '';
+  progress = 42;
   constructor() {}
 
+  setSelectedTab() {
+    console.log('Tabs select called');
+    this.selected = this.tabs.getSelected();
+  }
 }
