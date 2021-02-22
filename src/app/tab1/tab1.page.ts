1c5c85979dfaf332d7ad6566a95bb5943fb3bb5a
Jonathan Perry <jpper@umich.edu>
2021-02-22 22:59:18 +0900

    Finished his overview page & horizontal scroll section, as well as added some xcode opts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
+import heavyRotation from '../../assets/mockdata/heavyRotation.json';
+import jumpBackIn from '../../assets/mockdata/jumpBackIn.json';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  data = [
+    {
+      title: 'Recently played',
+      albums: recentlyPlayed,
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation,
+    },
+    {
+      title: 'Jump Back In',
+      albums: jumpBackIn,
+    },
+  ];
+
+  opts = {
+    sliderPerView: 2.4,
+    slidesOffsetBefore: 20,
+    spaceBetween: 20,
+    freeMode: true
+  }
 
   constructor() {}
 
+  // Image names
+  dasherize(string) {
+    return string.replace(/[A-Z]/g, function (char, index) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  }
 }
